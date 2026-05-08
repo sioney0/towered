fd3d82c4918996fc0ebd16c22bd2c227d4a64db2
sione <sione0718@gmail.com>
2026-05-08 21:27:00 +0900

    Fixed Jump Problem

--- main.lua
+++ main.lua
@@ -4,9 +4,11 @@ function love.load()
     wf = require "libraries/windfield" 
 
 
-    world = wf.newWorld(0, 100)
+    world = wf.newWorld(0, 250)
+    world:addCollisionClass("Ground")
     ground = world:newRectangleCollider(100, 400, 600, 100)
     ground:setType('static')
+    ground:setCollisionClass("Ground")
 
     player_one = Player:new(world, 100, 100, 100)
 end
--- player.lua
+++ player.lua
@@ -11,9 +11,11 @@ function Player:new(world, x_pos, y_pos, health)
         y = y_pos,
         hp = health,
         image = love.graphics.newImage('/sprites/Placeholder_Human.png'),
-        direction = 1
+        direction = 1,
+        canJump = true
+        
     }
-
+   
     entity.collider = world:newRectangleCollider(x_pos, y_pos, 32, 48)
     entity.collider:setFixedRotation(true)
 
@@ -28,28 +30,34 @@ function movePlayer(p, leftKey, rightKey, upKey, downKey)
     if love.keyboard.isDown('left') and px > -200 then
         p.collider:applyForce(-5000, 0)
         p.direction = -1
-    
     end
 
     if love.keyboard.isDown('right') and px < 200 then
         p.collider:applyForce(5000, 0)
         p.direction = 1
-
     end
-    if love.keypressed("up") and py > -200 then
-        p.collider:applyLinearImpulse(0, -900)
+
+    if love.keyboard.isDown("up") and py > -200 and p.canJump then
+        p.collider:applyLinearImpulse(0, -1000)
+        p.canJump = false
     end
 end
 
 function Player:update(dt)
     movePlayer(self, "left", "right", "up", "down")
-
+    if self.collider:enter("Ground") then
+        self.canJump = true
+    end
     self.x = self.collider:getX()
     self.y = self.collider:getY()
 end
 
 function Player:draw()
-    love.graphics.draw(self.image, self.x, self.y, 0, 20, 20)
+    local scale = 5
+    local imgW = self.image:getWidth()
+    local imgH = self.image:getHeight()
+
+    love.graphics.draw(self.image, self.x, self.y, 0, 5, 5)
 end
 
 return Player
